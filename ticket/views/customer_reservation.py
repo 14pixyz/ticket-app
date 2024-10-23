40f047b8ce50ef7cd985f76cd0be425b1577d098
14pixyz <ryo.pixy@gmail.com>
2024-10-23 09:44:33 +0900

    リファラの設定と予約確認ページへのアクセスリンクを作成

--- ticket/views/customer_reservation.py
+++ ticket/views/customer_reservation.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
-from ticket.models import Event, Ticket, Reservation
-from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from ticket.models import Reservation
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import redirect
 from ..forms import ReservationCreateForm
 
@@ -18,6 +18,15 @@ class ReservationListView(ListView):
     template_name = "customer/reservation-list.html"
     model = Reservation
     paginate_by = 5
+
+    def get(self, request, *args, **kwargs):
+        referer = request.META.get('HTTP_REFERER')
+        # リファラURLを指定
+        expected_referer = reverse('ticket:customer-query')
+        # リファラURLを含まない場合はリダイレクト
+        if not referer or expected_referer not in referer:
+            return redirect('ticket:customer-event-list')
+        return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
         customer_id = self.kwargs.get('customer_id')  # URLパラメータからcustomer_idを取得
